c2e24885d8dc30a609e1087b864891cf5b932157
James Pickard <james.pickard+github@gmail.com>
2013-01-18 16:43:49 -0500

    moving away from js-beautify as a node_module and into a lib

--- plugin/lib/beautify.js
+++ plugin/lib/beautify.js
@@ -1,41 +1,64 @@
-var jsbeautify = require('js-beautify').js_beautify,
-    fs = require('fs'),
-    path = require('path'),
-    argv = require('optimist')
-        .options('c', {
-        alias: 'config',
-        default: ''
-    })
-        .argv;
+var
+	fs = require('fs'),
+	path = require('path'),
+	validTypes = ['js', 'html', 'css'],
+	argv = require('optimist')
+		.usage ('Beautifully format a JavaScript, HTML, or CSS file.')
+		.describe('c', 'config file')
+		.default('c', '')
+		.alias('c', 'config')
+		.describe('t', 'file type; one of: js, html, css')
+		.alias('t', 'type')
+		.default('t', 'js')
+		.check( function (argv) {
+			if (argv._[0] === undefined){
+				throw 'Provide filename or - to read from stdin.';
+			}
+
+			if (argv._[0] !== '-' && fs.existsSync(argv._[0]) === false){
+				throw 'Input file ' + argv._[0] + ' does not exist.';
+			}
+
+			if (argv.c !== '' && fs.existsSync(argv.c) === false) {
+				throw 'Config file ' + argv.c + ' does not exist.';
+			}
+
+			if (validTypes.indexOf(argv.t) == -1) {
+				throw 'type must be one of: ' + validTypes.join(', ');
+			}
+		})
+		.argv;
 
 var filename = argv._[0],
-    configFile = argv.c,
-    opts = {};
+	configFile = argv.c,
+	type = argv.t,
+	opts = {};
 
 function getUserHome() {
-    return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-}
-
-if (filename === undefined) {
-    console.log('usage: ' + process.argv[1] + ' filename');
-    console.log(' or provide - as the filename to read from stdin.');
-    process.exit(1);
+	return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
 
 if (filename === '-') {
-    data = fs.readFileSync('/dev/stdin').toString();
+	data = fs.readFileSync('/dev/stdin').toString();
 } else {
-    data = fs.readFileSync(filename);
+	data = fs.readFileSync(filename);
 }
 
 if (configFile !== '') {
-    if (fs.existsSync(configFile)) {
-        opts = require(configFile)
-    } else {
-        process.stderr.write('configFile ' + configFile + ' does not exist');
-        process.exit(1);
-    }
+	opts = require(configFile)
 }
 
-var result = jsbeautify(data.toString(), opts);
+switch (type) {
+	case "js":
+		var beautify = require('./lib/js-beautify/beautify').js_beautify;
+		break;
+	case "html":
+		var beautify = require('./lib/js-beautify/beautify-html').html_beautify;
+		break;
+	case "css":
+		var beautify = require('./lib/js-beautify/beautify-css').css_beautify;
+		break;
+}
+
+var result = beautify(data.toString(), opts);
 console.log(result);
